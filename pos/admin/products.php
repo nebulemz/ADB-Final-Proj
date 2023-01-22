dcd97b7fff8a1ee4aab9dd1d21b8175c0d908af4
nebulemz <lemueljohnxd3@gmail.com>
2023-01-22 23:27:05 +0800

    can delete na

--- pos/admin/products.php
+++ pos/admin/products.php
@@ -4,12 +4,14 @@ include('config/config.php');
 include('config/checklogin.php');
 check_login();
 if (isset($_GET['delete'])) {
-  $id = intval($_GET['delete']);
-  $adn = "DELETE FROM  rpos_products  WHERE  prod_id = ?";
+  $id =($_GET['delete']);
+  $adn = "DELETE FROM  rpos_products  WHERE  prod_id =?";
   $stmt = $mysqli->prepare($adn);
   $stmt->bind_param('s', $id);
   $stmt->execute();
   $stmt->close();
+
+
   if ($stmt) {
     $success = "Deleted" && header("refresh:1; url=products.php");
   } else {
